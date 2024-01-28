487d0949dab004e5b9e43724498240ad4c45773b
Peter Wallis <p.a.e.wallis@gmail.com>
2024-01-28 10:41:49 -0600

    Fixed the spacing on the card grid

--- Scripts/cardObject.gd
+++ Scripts/cardObject.gd
@@ -1,11 +1,13 @@
-extends TextureRect
+extends AspectRatioContainer
+class_name CardUI
 
 var card_resource:Card
+@onready var panel:TextureRect = $Panel
+var icon:TextureRect
 
 func _get_drag_data(_at_position):
 	var preview_texture = TextureRect.new()
-	var child = get_child(1) #icon
-	preview_texture.texture = child.texture
+	preview_texture.texture = icon.texture
 	preview_texture.expand_mode = 1
 	preview_texture.size = Vector2(50, 50)
 	
@@ -15,9 +17,10 @@ func _get_drag_data(_at_position):
 	set_drag_preview(preview)
 	
 	var drag_data = {"card": card_resource}
-	texture = null
 	
 	return drag_data
 
-func set_card_resource(value):
+func set_card_resource(value:Card):
 	card_resource = value
+	icon = $Control/Icon
+	icon.texture = value.icon
--- Scripts/card_grid_populater.gd
+++ Scripts/card_grid_populater.gd
@@ -3,22 +3,25 @@ extends Node
 @export var cards:Array[Card] = []
 @export var grid:GridContainer
 
+var scene_to_instance
+
 func _ready():
-	var i = 0
+	# We probably only need to load this once
+	scene_to_instance = load("res://Scenes/UIScenes/card.tscn")
+	
+	
+	#var i = 0
 	for card in cards:
-		if i > 1: 
-			return
+		#if i > 1: 
+		#	return
 		if card is Card:
-			print("Available card:" + card.word)
-			instance_game_object(card.icon, card)
-			i += 1
+			#print("Available card:" + card.word)
+			instance_game_object(card)
+			#i += 1
 			pass
 
 # Function to instance and add a GameObject at runtime
-func instance_game_object(icon, card):
-	var scene_to_instance = load("res://Scenes/card.tscn")
+func instance_game_object(card:Card):
 	var instance = scene_to_instance.instantiate()
 	instance.set_card_resource(card)
-	var childnode = instance.get_child(1)
-	childnode.texture = icon
 	grid.add_child(instance)
