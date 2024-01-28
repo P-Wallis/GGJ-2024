97aba90af892a21844b05cc23f4a8cec105b4a0e
ethanfischer <ethan.fischer@incontextsolutions.com>
2024-01-27 23:21:47 -0600

    Yes it's recognizing what you drop in the slot again and showing the icon!

--- Scripts/actionslot.gd
+++ Scripts/actionslot.gd
@@ -3,13 +3,12 @@ extends TextureRect
 var card:Card
 
 func _can_drop_data(_at_position, data):
-	var isit = typeof(data) == TYPE_DICTIONARY and "texture" in data and data["texture"] is Texture2D
-	print("isit" + isit)
-	return isit
+	return typeof(data) == TYPE_DICTIONARY and "card" in data and data["card"] is Card
 
 func _drop_data(_at_position, data):
 	if "card" in data:
 		card = data["card"]
+		texture = card.icon
 		
 func _ready():
 	# Add this node to the "UI/action_slots" group
