167ee1fca9c8b054eebc7be8d4097ea892d60b4b
ethanfischer <ethan.fischer@incontextsolutions.com>
2024-01-27 18:55:55 -0600

    Third slot

--- Scripts/actionslotservice.gd
+++ Scripts/actionslotservice.gd
@@ -14,6 +14,6 @@ func loop_through_action_slots_and_print_names():
 	print("got action slots")
 	for action_slot in action_slots:
 		if action_slot.CardInSlotName != "":
-			print("CardNames:" + action_slot.CardInSlotName)
+			print(action_slot.CardInSlotName)
 		else:
 			print("No card name set for this slot.")
