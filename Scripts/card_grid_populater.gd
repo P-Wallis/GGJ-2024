ef8d0f06a3bffb206d66e4ee73bca9f69218b2f0
ethanfischer <ethan.fischer@incontextsolutions.com>
2024-01-27 22:47:28 -0600

    We're getting there!

--- Scripts/card_grid_populater.gd
+++ Scripts/card_grid_populater.gd
@@ -4,15 +4,20 @@ extends Node
 @export var grid:GridContainer
 
 func _ready():
+	var i = 0
 	for card in cards:
+		if i > 1: 
+			return
 		if card is Card:
 			print("Available card:" + card.word)
 			instance_game_object(card.icon)
+			i += 1
 			pass
 
 # Function to instance and add a GameObject at runtime
 func instance_game_object(icon):
 	var scene_to_instance = load("res://Scenes/card.tscn")
 	var instance = scene_to_instance.instantiate()
-	instance.texture = icon
+	var childnode = instance.get_child(1)
+	childnode.texture = icon
 	grid.add_child(instance)
