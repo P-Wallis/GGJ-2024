b2af2e6239a37ead9fdfbabf7a9c2a2ad2e3a831
ethanfischer <ethan.fischer@incontextsolutions.com>
2024-01-27 18:48:23 -0600

    actionslotservice is printing contents after 5 seconds

--- Scripts/actionslot.gd
+++ Scripts/actionslot.gd
@@ -1,12 +1,18 @@
 extends TextureRect
 
+var CardInSlotName = ""
+
 func _can_drop_data(at_position, data):
 	return typeof(data) == TYPE_DICTIONARY and "texture" in data and data["texture"] is Texture2D
 
 func _drop_data(at_position, data):
-	print("hello world")
+
 	if "texture" in data:
 		texture = data["texture"]
 	if "meta" in data:
 		var cardname = data["meta"]
-		print(cardname)
+		CardInSlotName = cardname
+		
+func _ready():
+	# Add this node to the "UI/action_slots" group
+	add_to_group("action_slots")
---
+++ Scripts/actionslotservice.gd
@@ -0,0 +1,19 @@
+extends Node
+
+func _ready():
+	await loop_through_action_slots_and_print_names()
+
+func loop_through_action_slots_and_print_names():
+	# Wait for 5 seconds before starting the loop.
+	var timer = get_tree().create_timer(5)
+	await timer.timeout
+
+
+	# Now that 5 seconds have passed, get all action slots.
+	var action_slots = get_tree().get_nodes_in_group("action_slots")
+	print("got action slots")
+	for action_slot in action_slots:
+		if action_slot.CardInSlotName != "":
+			print("CardNames:" + action_slot.CardInSlotName)
+		else:
+			print("No card name set for this slot.")
