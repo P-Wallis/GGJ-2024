ada49160a016676a5671edb26a4242fc3fbae41f
Peter Wallis <p.a.e.wallis@gmail.com>
2024-01-28 14:33:35 -0600

    Moved to using ideas, rather than card words

--- Scripts/UI/thought_bubble_ui.gd
+++ Scripts/UI/thought_bubble_ui.gd
@@ -14,6 +14,10 @@ func getFailMessage(cards:Array[Card]):
 	var info = pairInfo.GetEmotionInfo(cards)
 	var mood:CardPairs.Emotion = info["mood"]
 	var pairs = info["cardPairs"]
+	var ideas = info["ideas"]
+	var idea:String = "nothing in particular"
+	if(ideas[mood].size() > 0):
+		idea = ideas[mood][randi() % ideas[mood].size()]
 	var moodPair:Array[Card] = [cards[0], cards[1]]
 	if(pairs[mood].size() > 0):
 		moodPair = pairs[mood][randi() % pairs[mood].size()]
@@ -29,19 +33,19 @@ func getFailMessage(cards:Array[Card]):
 	var sentence = "I don't understand."
 	match mood:
 		CardPairs.Emotion.CONFUSED:
-			sentence = AddCardWordsToMessage(
-				"Huh? What does %s have to do with %s? I don't get it.",
-				moodPair)
+			#sentence = AddCardWordsToMessage("Huh? What does %s have to do with %s? I don't get it.",moodPair)
+			sentence = AddIdeaToMessage("...%s? I don't get it.",idea)
 		CardPairs.Emotion.AMUSED:
-			sentence = AddCardWordsToMessage(
-				"Haha! %s and %s! Most amusing!",
-				moodPair)
+			#sentence = AddCardWordsToMessage("Haha! %s and %s! Most amusing!",moodPair)
+			sentence = AddIdeaToMessage("Haha! It's %s! Most amusing!",idea)
 		CardPairs.Emotion.OFFENDED:
-			sentence = AddCardWordsToMessage(
-				"%s and %s! How dare you mention that to me!?",
-				moodPair)
+			#sentence = AddCardWordsToMessage("%s and %s! How dare you mention that to me!?",moodPair)
+			sentence = AddIdeaToMessage("How dare you mention %s to me!?",idea)
 	
 	return sentence
 
 func AddCardWordsToMessage(inputMessage:String, cards:Array[Card]):
 	return inputMessage % [cards[0].word, cards[1].word]
+	
+func AddIdeaToMessage(inputMessage:String, idea:String):
+	return inputMessage % idea
