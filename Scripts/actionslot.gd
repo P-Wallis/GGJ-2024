c1bcc3da15fedf5f3d6d2ab9a90279c0750b4a75
ethanfischer <ethan.fischer@incontextsolutions.com>
2024-01-27 23:15:21 -0600

    WIP fixing drag and drop

--- Scripts/actionslot.gd
+++ Scripts/actionslot.gd
@@ -3,12 +3,11 @@ extends TextureRect
 var card:Card
 
 func _can_drop_data(_at_position, data):
-	return typeof(data) == TYPE_DICTIONARY and "texture" in data and data["texture"] is Texture2D
+	var isit = typeof(data) == TYPE_DICTIONARY and "texture" in data and data["texture"] is Texture2D
+	print("isit" + isit)
+	return isit
 
 func _drop_data(_at_position, data):
-
-	if "texture" in data:
-		texture = data["texture"]
 	if "card" in data:
 		card = data["card"]
 		
