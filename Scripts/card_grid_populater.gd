97d451547da6ad54ecdfcf9daca3a5adee6e0def
ethanfischer <ethan.fischer@incontextsolutions.com>
2024-01-27 21:08:47 -0600

    Assign icons to each carddata and fix card_grid_populater compiler errors

--- Scripts/card_grid_populater.gd
+++ Scripts/card_grid_populater.gd
@@ -6,12 +6,13 @@ func _ready():
 	for card in cards:
 		if card is Card:
 			print("Available card:" + card.word)
-			instance_game_object()
+			instance_game_object(card.icon)
 			pass
 
 # Function to instance and add a GameObject at runtime
-func instance_game_object():
+func instance_game_object(icon):
 	var scene_to_instance = load("res://Scenes/card.tscn")
 	var instance = scene_to_instance.instantiate()
+	instance.texture = icon
 	add_child(instance)
 
