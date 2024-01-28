67ba9f2d287ccb52530fc022b3c95da6786fa245
ethanfischer <ethan.fischer@incontextsolutions.com>
2024-01-27 18:19:08 -0600

    Passing data from card to slot

--- Scripts/actionslot.gd
+++ Scripts/actionslot.gd
@@ -1,12 +1,12 @@
 extends TextureRect
-	
+
 func _can_drop_data(at_position, data):
-	return data is Texture2D
-	
+	return typeof(data) == TYPE_DICTIONARY and "texture" in data and data["texture"] is Texture2D
+
 func _drop_data(at_position, data):
 	print("hello world")
-	texture = data
-	var cardname = get_meta("Name")
-	print(cardname)
-
-
+	if "texture" in data:
+		texture = data["texture"]
+	if "meta" in data:
+		var cardname = data["meta"]
+		print(cardname)
