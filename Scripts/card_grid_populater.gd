203ffd1ad8e0a006e8d67d87f5664389392af00e
ethanfischer <ethan.fischer@incontextsolutions.com>
2024-01-27 21:00:42 -0600

    WIP instantiating scenes

--- Scripts/card_grid_populater.gd
+++ Scripts/card_grid_populater.gd
@@ -6,4 +6,12 @@ func _ready():
 	for card in cards:
 		if card is Card:
 			print("Available card:" + card.word)
+			instance_game_object()
 			pass
+
+# Function to instance and add a GameObject at runtime
+func instance_game_object():
+	var scene_to_instance = load("res://Scenes/card.tscn")
+	var instance = scene_to_instance.instantiate()
+	add_child(instance)
+
